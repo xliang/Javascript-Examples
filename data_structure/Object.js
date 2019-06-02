4b08adc1fdc60fc717c3bbf98b7db4532268a6a2
Liang <liangwu2004@gmail.com>
2019-06-02 01:06:24 -0400

    add Object.Create method

--- data_structure/Object.js
+++ data_structure/Object.js
@@ -12,3 +12,26 @@ console.log(Object.keys({ "tab1": "1" , tab2: "2"})[1]);
 
 console.log(Object.keys({ "tab1": "1" , tab2: "2"})[2]);
 
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+console.log(Object.keys(object1));
+
+/* Object Create 
+
+*/
+var proto = {
+	sentence : 4,
+	probation : 2
+}; 
+
+var first = Object.create(proto); 
+first.name = 'Joe';
+first.id = '12A'; 
+
+var second = Object.create(proto); 
+second.name = 'Sam';
+second.id = '2BC';
