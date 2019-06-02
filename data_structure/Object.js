48abbfbece75bae813eb7414961775c08af33add
Liang <liangwu2004@gmail.com>
2019-06-02 00:50:25 -0400

    add Object.key tests

---
+++ data_structure/Object.js
@@ -0,0 +1,14 @@
+
+/* 
+    Object.keys 
+    method returns an array of a given object's own property names, in the same order as we get with a normal loop.
+*/
+
+console.log(Object.keys({ "tab1": "1" , tab2: "2"})); 
+
+console.log(Object.keys({ "tab1": "1" , tab2: "2"})[0]);
+
+console.log(Object.keys({ "tab1": "1" , tab2: "2"})[1]);
+
+console.log(Object.keys({ "tab1": "1" , tab2: "2"})[2]);
+
---
+++ function/async/async function.js
@@ -0,0 +1,28 @@
+// Async arrow function 
+
+const foo = async () => {
+    // do something 
+}
+
+// Async arrow functions for single argument 
+
+const foo = async evt => {
+
+}
+
+
+// anonymous form 
+const fool = async function ()  {
+
+}
+
+// An async function declaration 
+async function foo() {
+
+}
+
+// using async function in a callback
+const foo = event.onCall (async () => {
+    // do something 
+}); 
+
