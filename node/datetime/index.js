a4aa9e9c2f86dba4553fc219a38675eca6bfad8c
xliang <liangwu2004@gmail.com>
2019-06-07 22:14:40 -0500

    modify sparetime module version

--- node/datetime/index.js
+++ node/datetime/index.js
@@ -1,4 +1,5 @@
-const spacetime = require('spacetime')
-let data_value = '12/25/1996'
+const spacetime = require('spacetime');
+let data_value = '12/25/1996';
+let iso_data = spacetime(data_value).format('iso');
 
-console.log(spacetime(data_value).format('iso')); 
+console.log(iso_data);
