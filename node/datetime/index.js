64670b56d32d47ddd405016ebeee328b11105008
xliang <liangwu2004@gmail.com>
2019-06-08 23:28:09 -0500

    change file to text authentication

--- node/datetime/index.js
+++ node/datetime/index.js
@@ -1,3 +1,4 @@
+/* version 4.3.0 will use current date time for iso date if date time missing in the string */
 const spacetime = require('spacetime');
 let data_value = '12/25/1996';
 let iso_data = spacetime(data_value).format('iso');
